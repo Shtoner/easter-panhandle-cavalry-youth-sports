6195bcb2a82e79672c6aa21ba07db75766db2731
Shtoner <zstoner57@gmail.com>
2023-01-20 16:15:33 -0600

    beginning template

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import easterpanhancav from './easternpanhancav.jpg'
+import eastercav from './easternpancav.jpg'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <img src={eastercav} className='logo' alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <a href='https://www.facebook.com/groups/444599141095663'>
+        <h2>Check out our Facebook page:</h2>
         </a>
+        <div>
+          <h2>TRYOUTS!</h2>
+        </div>
+        <img src={easterpanhancav} className="App-logo" alt="logo" />
+
+        <div>
+          <h2>FAQs</h2>
+          -Eastern Panhandle Cavalry is a travel team competing in the American Youth Football league (AYF)  NOT RECREATION🚫⛔️
+-Athlete must attend tryouts! (January 28, 2022) 10am/ location TBD
+-ONLY 7U/8U SQUADS are participating in spring leagues. All recruitment is for 2023 Fall Season
+-Whatever age the athlete is on July 31 will be the age group that athlete will play in.
+-Away games are at least 1.5 hours up to 3 hours away from martinsburg. Playoffs travel distance is unlimited.
+-Practice will be in Berkeley County/Martinsburg 
+-unlimited weight limit league.
+-our WEEKLY winter workouts are and always have been FREE!!! 
+-No people are not just coming over and joining team, and staff. All parties are selected and chosen. 
+-As we love to teach life lessons through football, WE the EP CAVALRY ARE ABSOLUTELY COMPETING TO WIN!
+        </div>
+        <div>
+          <h2>Shout Out Our Sponsors!</h2>
+    <ul>
+      <li>Cardinal Realty Group Inc.</li>
+      <li>Negley's Water and Well Drilling</li>
+      <li>Johnathan Knight</li>
+    </ul>
+        </div>
       </header>
     </div>
   );
