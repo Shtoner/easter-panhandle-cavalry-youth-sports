112e0ff75370cbdb559d3e5aa65942faf29337de
Shtoner <zstoner57@gmail.com>
2023-01-30 13:07:24 -0600

    conference captions

--- src/Conference.js
+++ src/Conference.js
@@ -22,16 +22,27 @@ export default function Conference(){
 
             <header className="App-header">
 <img src={pic1} className='App-header2'/>
+<h3 className='brandname'>Dmv Hammers-Aldie, VA</h3>
 <img src={pic2} className='App-header2'/>
+<h3 className='brandname'>Aim High Panthers-Centreville, VA</h3>
 <img src={pic3} className='App-header2'/>
+<h3 className='brandname'></h3>
 <img src={pic4} className='App-header2'/>
+<h3 className='brandname'>Virginia Knights-Stafford, VA</h3>
 <img src={pic5} className='App-header2'/>
+<h3 className='brandname'>Virginia Jags-Fredericksburg, VA</h3>
 <img src={pic6} className='App-header2'/>
+<h3 className='brandname'>Spotsy Cougars-Spotslyvania, VA</h3>
 <img src={pic7} className='App-header2'/>
+<h3 className='brandname'>South County Colts-Lorton, VA</h3>
 <img src={pic8} className='App-header2'/>
+<h3 className='brandname'>LoCo War Eagles-Purcelville,VA</h3>
 <img src={pic9} className='App-header2'/>
+<h3 className='brandname'>Common Wealth Warriors-Manasses, VA</h3>
 <img src={pic10} className='App-header2'/>
+<h3 className='brandname'>Alexandria Titans-Alexandria, VA</h3>
 <img src={pic11} className='App-header2'/>
+<h3 className='brandname'>703 United-Fairfax, VA</h3>
             </header>
         </div>
     )
