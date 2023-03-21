bac0a6afbaa7b84444820cf2c9214684ca35adf1
Shtoner <zstoner57@gmail.com>
2023-03-20 21:41:03 -0500

    rev

--- src/Contact.js
+++ src/Contact.js
@@ -1,15 +1,21 @@
 import Logo from './image0.png'
 
-import './Home.css'
 export default function Contact(){
     return(
     <div className='App'>
     <img src={Logo} className='logo' alt="logo" />
         <header className='App-header'>
             <h1>Contact Us!</h1>
+            <br></br>
+            <br></br>
             <h2>Rod Holloway</h2>
             <h3>Roderick.j.Holloway@gmail.com</h3>
             <h2>304-620-1743</h2>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>       
+            <br></br>       
         </header>
 
     </div>
